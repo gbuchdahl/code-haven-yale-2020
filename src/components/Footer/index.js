171a059418f04ccb29d768260cd8e38bd0488886
Gabriel Buchdahl <gbuchdahl@gmail.com>
2020-03-23 17:57:15 -0400

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,6 +9,7 @@ const Footer = () => {
           <p>
             {config.copyright}
           </p>
+          <p>Add us on <a href='https://facebook.com/codehavenyale'>Facebook</a>, and check us out on <a href='https://medium.com/code-haven'>Medium!</a></p>
           <p>Powered by <a href='https://www.gatsbyjs.org'>Gatsby</a> and <a href='https://www.netlifycms.org'>Netlify CMS</a></p>
         </div>
       </div>
