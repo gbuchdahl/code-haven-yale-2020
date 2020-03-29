4eb639f4f16d2ae4a1930e4a87316083e8735fad
maansi-dasari <mdbidda@gmail.com>
2020-03-29 18:54:51 -0400

    Update index.js

--- src/components/People/index.js
+++ src/components/People/index.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 const People = ({ gridItems }) => (
   <div className='columns is-multiline'>
     {gridItems.map(item => (
-      <div key={item.image} className='column is-3 is-half-mobile has-text-centered' style={{ borderRadius: '3px' }}>
+      <div key={item.image} className='column is-3 is-half-mobile has-text-centered' style={{ borderRadius: '5px' }}>
         <section className='section has-text-centered'>
-          <div className='container is-128x128 image is-rounded'>
+          <div className='container is-128x128 image box'>
             {/* <p className='box image is-128x128'> */}
             <img className='is-rounded' alt='' src={item.image} />
             {/* </p> */}
